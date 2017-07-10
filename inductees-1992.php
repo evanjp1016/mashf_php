4f3aef9e95e770c61d3417068999f499bece34e2
Evan Pesch <epesch@umich.edu>
2017-07-10 16:40:56 -0400

    Upload inductees-1992

---
+++ inductees-1992.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+	<title>MASHF - 1992 Inductees</title>
+	<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+	<?php include "nav.php" ?>
+
+	<div>
+		<h2>1992 Inductees</h2>
+	</div>
+
+	<div>
+		<h2>Hilliard (Gudelsky) Gates</h2>
+		<h3>Born: December 14, 1915, Muskegon, MI</br>Died: November 20, 1996, Fort Wayne, IN</h3>
+
+		<p>A graduate of Muskegon High School, Hilliard Gates' 52 years in sportscasting established himself as one of the finest in the profession.  Christened Hilliard Gudelsky, he adopted the last name Gates when he got his start in broadcasting at WKBZ radio in Muskegon in 1936.  In June 1940, the Muskegon native moved to Fort Wayne to take a job with WOWO radio, a powerful AM station.  Gates switched to Fort Wayne's WKJG in 1947, where he branched into television and administration.</p>
+		<p>While in Fort Wayne, Gates described the games of the National Basketball Association's Zollner Pistons for 10 years prior to their move to Detroit in 1957.  His abilities behind the microphone led to network recognition with his coverage of the first NBA All‑Star Game for the Mutual Broadcasting System in 1951.  In 1966 and 1967, he worked on radio coverage of Rose Bowl games for NBC.  Showing his diverse talents, he handled live coverage of the Indianapolis 500 for the Canadian Television Network for nine years.</p>
+		<p>While Gates did the play‑by‑play coverage of Indiana University basketball for cable television, he is best known for his years of service as the voice of Indiana High School boy's and girl's basketball championships.  He was nominated 20 times for the "Outstanding Sports Broadcaster" award and won the award seven times.  He was the first broadcaster inducted into the Indiana Basketball Hall of Fame, receiving the Silver Medal Award in 1969.</p>
+		<p> His experience led him behind the microphone for the 1987 film "Hoosiers," a David and Goliath story based on the 32‑30 upset of powerful Muncie Central by tiny Milan High School for the Indiana State Championship in 1954.  His role was a logical choice, since he had announced the play‑by‑play account of the actual Milan/Muncie classic.</p>
+	</div>
+
+	<div>
+		<h2>Ray Castenholz</h2>
+		<h3>Born: April 19, 1896, Muskegon, MI</br>Died: October 10, 1984, Muskegon, MI</h3>
+
+		<p>Ray Castenholz, like his Muskegon ancestors (an early local baseball park was called Castenholz Park), was an influential force in promoting and preserving the sport of baseball in the Port City.  One of the founders of the Greater Muskegon Little League, Castenholz served as president of the organization from 1952 through 1959.</p>
+		<p>A three‑time All‑State end on the gridiron at Muskegon High School, Castenholz also established a Greater Muskegon track record of 53.4 seconds in the 440‑yard dash in 1912. On the baseball diamond, Castenholz also excelled.  A catcher for a variety of teams, he was once considered a major league prospect.  However, he shunned the spotlight to attend Michigan Agricultural College (now Michigan State University).  After college he returned home to the family's 80‑acre fruit farm.</p>
+		<p>A skilled golfer and an excellent bowler, Castenholz's interest in sports remained strong through the years.  During his free time, he helped develop an improved statistical rating system that utilized "average performance" charts for ranking college football teams.  Many of the features of the system are still in use today.</p>
+	</div>
+
+	<div>
+		<h2>Jim Johnson</h2>
+		<h3>Born: November 3, 1945, Muskegon, MI</br>Died: December 6, 1987, North Muskegon, MI</h3>
+
+		<p>Muskegon High School's Jim Johnson was possibly the last baseball superstar to benefit from the tutelage of the Big Reds' veteran coach Harry Potter, who retired from the school in 1966.  Blessed with a strong arm and impeccable fielding skills, Johnson also starred as a pitcher and center fielder with the talented Muskegon Pepsis of the United Baseball League during the summer months.</p>
+		<p>The team leader at the plate with a .386 batting average, Johnson led the Big Reds to a 21‑1 season and the Lake Michigan Athletic Conference crown during the 1963 season.  He earned an academic scholarship to Western Michigan University.</p>
+		<p>At Western, Johnson also split time between the mound and the outfield.  Johnson carried a .415 batting average and a 5‑2 pitching mark with a 1.71 earned run average during his junior year.  In 1966, the Broncos ended with a 21‑5 record paced by the southpaw Johnson's 7‑0 record, including five shutouts in eight appearances.  His 0.51 ERA in 70 innings ranked third in the nation.  He was named to the Mid‑American Conference first team and was selected to the College All‑American second team.</p>
+		<p>Johnson was drafted by the Houston Astros and Detroit Tigers, but chose to complete his studies at WMU before joining the professional ranks.  In June 1967, he signed with the San Francisco Giants and was assigned to Medford, Oregon.  He also made stops in Salt Lake City and Amarillo, Texas before joining the Giants' AAA Phoenix Club in 1969.</p>
+		<p>Johnson was promoted to the Giants' roster in October 1969.  He opened the 1970 season as a member of the major league club. In his only major league decision, Johnson allowed one hit in 1 1/3 innings to beat the Cincinnati Reds.  He was later returned to Phoenix where he ended with a 6‑5 record.</p>
+		<p>After retiring from baseball in 1970, Johnson entered the field of education.  In 1983, he accepted the position of uperintendent of North Muskegon Schools.  He died of cancer in 1987 at the age of 42.</p>
+	</div>
+
+	<div>
+		<h2>William "Wild Bill" McCall</h2>
+		<h3>Born: July 29, 1909, Muskegon, MI</br>Died: October 6, 1988, Dayton, OH</h3>
+
+		<p>A two‑time All‑Stater at Muskegon High School, Bill McCall led the Big Reds to state football titles in 1926 and 1927.  He was selected by The Detroit News as quarterback of their third team in 1926, and earned first‑team honors one year later, as a senior.</p>
+		<p>On the basketball court, McCall led Muskegon to a 20‑1 record and a Class A state championship in the spring of 1927.  After the state tourney, the Big Reds traveled to Chicago to compete in the University of Chicago's National Cage Tournament, losing to Batesville High School of Arkansas in the second round.</p>
+		<p>McCall moved on to Dartmouth University, where he earned the nickname “Wild Bill” because of his reputation as an "all‑purpose threat."  He showed his versatility in a 33‑all tie against Yale in 1931, scoring three touchdowns, including a 93‑yard kickoff return, an 85‑yard pass reception and a 65‑yard interception return.  McCall, who scored 90 points during his senior year, was a strong candidate for Grantland Rice's All‑American team.  His point total ranked fourth on Dartmouth's all‑time list.</p>
+		<p>McCall also led Dartmouth's basketball team to a 15‑6 record during his senior year.  In three years of cage action, he scored 146 points in 31 games.</p>
+	</div>
+
+	<div>
+		<h2>Gil and Gordon McKellen</h2>
+		<h3>Gil - Born: March 28, 1916, Glenn, MI</br>Died: July 29, 2001, Denver, CO</br>
+			Gordon - Born: October 10, 1918, Muskegon, MI</br>Died: August 21, 1997, Dallas, TX</h3>
+
+		<p>The McKellen brothers began their rise to stardom by spending countless hours as "rink rats" on the ice of Muskegon's old Mart Auditorium in the 1930s.  Those many hours of strenuous skating gave the McKellens the opportunity to create a unique act of tumbling that attracted the attention of Sonja Henie, three‑time Olympic Champion.  They were offered a contract with the Henie's Ice Revue, but turned it down because they were still in high school.</p>
+		<p>Three years later, the Ice Follies brought their show to Muskegon's Mart.  Between acts, the McKellens performed for the management of the show and were offered a contract, which they accepted in February 1938.  Jumps, flips and tumbling acts were a part of the McKellens' program.  It also included a hand‑to‑hand stand in which Gilbert would carry Gordon above his head around the ice.</p>
+		<p>In 1943, the brothers hung up their skates and joined the United States Army.  Upon their discharge in 1946, the McKellens went back to work with the Ice Follies.  A year later, they joined Tom Arnold's Stars on Ice.  A trip to London included a performance for England's royal family.  In 1948, the brothers signed with Henie's Ice Revue.  They performed four shows a day in New York over a three‑week period.</p>
+		<p>They returned to the West Michigan area in 1954 to skate before 6,000 spectators in the Grand Rapids Stadium Arena.  The McKellen brothers retired in December 1955, but they did reunite for one last local encore performance at Muskegon's L. C. Walker Arena with the Ice Varieties in the 1960s.</p>
+		<p>Gordon's son, Gordon McKellen Jr. was a US Men's Figure Skating champion in the mid-1970s.</p>
+	</div>
+
+	<div>
+		<h2>Cliff Taylor</h2>
+		<h3>Born: February 28, 1928, Spring Lake, MI</h3>
+
+		<p>A 13‑time winner of the Muskegon District Amateur Golf Tournament, Cliff Taylor  dominated the local golf for 40 years.</p>
+		<p>In 1950, at the age of 22, Taylor fired a two-round 146 for his first district crown.  The Spring Lake resident added a second title in the spring of 1957.  A two‑round sudden death victory in the 1960 tournament was the beginning of a seven‑year championship run by Taylor.  Among the consecutive crowns was a record‑breaking four‑under par 140 in 1965.  A victory in 1968 pushed his total to 10 district titles in the 20-year history of the event.  Two-stroke victories in 1976 and in 1978 boosted Taylor's total to 12 districts.  From that  point on, he began concentrating on the senior amateur tournaments, having turned 50 in 1978.</p>
+		<p>In 1983, Taylor missed the U.S. Senior Amateur by one stroke.  He advanced to the final eight in 1984.  In 1985, he reached the semifinal round.  That accomplishment gave him a spot in the United States Golf Association's 1986 U.S. Senior Open in Columbus, Ohio.  With a two‑round score of 153, Taylor advanced to the final two rounds.  He also has competed in the British Senior Amateur Tournament and the U.S. Senior Amateur.</p>
+		<p>In 1991, Taylor returned to win his 13th district title against a field crowded with younger players.  The 63‑year old emerged with a one‑over‑par 145 to win the event.  Later that year, the Golf Association of Michigan named him to the first Senior Honor Roll.</p>
+	</div>
+
+	<div>
+		<h2>Bill Wolski</h2>
+		<h3>Born: May 23, 1944, Muskegon, MI</br>Died: March 5, 2006, Atlanta, GA</h3>
+
+		<p>Captain of the 1961 Muskegon Catholic Central gridiron team, Bill Wolski earned first‑team All‑State honors as a running back.  Following graduation, the 5-10, 194-pound prep star continued his football career at Notre Dame.  Wolski broke into the Irish starting lineup in the third game of the 1963 season.  The sophomore back notched 87 yards in 16 carries as Notre Dame defeated Southern California 17‑14.</p>
+		<p>The following season, the Irish rolled to a 9‑1 mark under the guidance of new coach Ara Parseghian.  The junior halfback Wolski led the team in rushing with 657 yards in 136 attempts and in scoring with 11 touchdowns.  A 20‑17 loss to USC in the final game kept the No. 1 Irish from claiming the national championship.</p>
+		<p>In 1965, the Irish posted a respectable 6‑5‑1 record.  Wolski's eight touchdowns and a pair of two‑point conversions gave him a team-leading 52 points.  His five‑touchdown performance against Pittsburgh that year ranks second in the all‑time Notre Dame record book.  He also tied Paul Hornung for most kickoff returns in a season with 16.</p>
+		<p>Following graduation, Wolski was named to the roster of the East‑West Shrine game.  The NFL Atlanta Falcons drafted him in the fifth round.  He also received an offer from the New York Jets of the American Football League.  Wolski signed with the Falcons, but an injury to his right knee in pre‑season drills sidelined him for much of the 1966 season.  He returned to Atlanta following successful off‑season surgery, but in late July, he then injured his left knee.  After the 1967 season, Wolski was released at his request and he retired from pro football.</p>
+	</div>
+</body>
+</html>
